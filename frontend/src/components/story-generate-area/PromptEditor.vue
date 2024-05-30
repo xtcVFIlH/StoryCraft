0bc94826c8095453d9653c49e92cf976fb1b312f
chenlong <hoccl@sina.com>
2024-05-30 10:29:51 +0800

    简化提示词输入组件

--- frontend/src/components/story-generate-area/PromptEditor.vue
+++ frontend/src/components/story-generate-area/PromptEditor.vue
@@ -1,46 +1,24 @@
 <template>
     <div class="container">
-        <div class="input-container">
-            <el-input
-                type="textarea"
-                :rows="5"
-                resize="none"
-                v-model="prompt"
-                :autosize="{ minRows: 1, maxRows: 5 }"
-                placeholder=""
-                @keydown.enter="handleSubmit"
-                @input="handleInput"
-            ></el-input>
-        </div>
-        <div class="btn-container">
-            <el-button
-                type="primary"
-                :icon="Upload"
-                @click="handleSubmit"
-                size="small"
-                circle
-            >
-            </el-button>
-        </div>
+        <el-input
+            type="textarea"
+            :rows="2"
+            resize="none"
+            v-model="prompt"
+            placeholder="输入新故事情节，按回车键发送"
+            @keydown.enter="handleSubmit"
+            @input="handleInput"
+            :inputStyle="{ borderRadius: 0 }"
+        ></el-input>
     </div>
 </template>
   
 <script setup>
 import { ref, watch } from 'vue'
-import { ElNotification } from 'element-plus'
-import { Upload } from '@element-plus/icons-vue'
 
 const props = defineProps([
     'userPromptClearCount',
 ])
-
-const alertError = error => {
-    ElNotification({
-        title: '错误',
-        message: error,
-        type: 'error',
-    });
-}
 
 const prompt = ref('')
 
@@ -53,43 +31,15 @@ watch(() => props.userPromptClearCount, () => {
 const handleSubmit = () => {
     const promptEntered = prompt.value.trim();
     if (!promptEntered) {
-        alertError('用户提示词不能为空');
-        return;
-    }
-    if (promptEntered.includes('---') || promptEntered.includes('```')) {
-        alertError('用户提示词不能包含---、```');
         return;
     }
     emit('submit', promptEntered)
 }
 
-const handleInput = (value) => {
-    value = value.replace(/[\r\n]+/g, '');
-
-    if (value.length > 200) {
-        value = value.slice(0, 200);
-    }
-
-    prompt.value = value;
-};
+const handleInput = value => {
+    prompt.value = value.replace(/[\r\n]/g, '')
+}
 </script>
 
 <style scoped>
-.container {
-    box-sizing: border-box;
-    display: flex;
-    align-items: center;
-    padding: 5px 0 0 0;
-}
-.input-container {
-    flex-grow: 1;
-    flex-shrink: 1;
-}
-.btn-container {
-    flex-grow: 0;
-    flex-shrink: 0;
-    box-sizing: border;
-    margin: 0 0 0 5px;
-
-}
 </style>
