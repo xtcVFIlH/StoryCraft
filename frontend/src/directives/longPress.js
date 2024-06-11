9ce133da8eed1e8bb905e22f9b768efdc873958a
chenlong <hoccl@sina.com>
2024-06-11 17:32:11 +0800

    添加移动端长按的自定义指令

---
+++ frontend/src/directives/longPress.js
@@ -0,0 +1,46 @@
+const startKey = `__longPressStart_${Math.random().toString(36).substr(2, 9)}`;
+const cancelKey = `__longPressCancel_${Math.random().toString(36).substr(2, 9)}`;
+
+const longPress = {
+    mounted(el, binding) {
+        if (typeof binding.value !== 'function') {
+            throw 'callback must be a function';
+        }
+        let pressTimer = null;
+
+        const start = (e) => {
+            if (e.type === 'click' && e.button !== 0) {
+                return;
+            }
+            if (pressTimer === null) {
+                pressTimer = setTimeout(() => {
+                    binding.value(e);
+                }, 500);
+            }
+        };
+
+        const cancel = () => {
+            if (pressTimer !== null) {
+                clearTimeout(pressTimer);
+                pressTimer = null;
+            }
+        };
+
+        el[startKey] = start;
+        el[cancelKey] = cancel;
+    
+        el.addEventListener("touchstart", start);
+        el.addEventListener("touchend", cancel);
+        el.addEventListener("touchcancel", cancel);
+    },
+    unmounted(el) {
+        const start = el[startKey];
+        const cancel = el[cancelKey];
+
+        el.removeEventListener("touchstart", start);
+        el.removeEventListener("touchend", cancel);
+        el.removeEventListener("touchcancel", cancel);
+    }
+};
+
+export default longPress;
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,12 +4,15 @@ import 'element-plus/dist/index.css';
 import App from './App.vue';
 
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+import longPress from './directives/longPress';
 
 const app = createApp(App);
 
 for (const [componentName, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(componentName, component);
 }
+
+app.directive('longpress', longPress);
 
 app.use(ElementPlus);
 app.mount('#app');
