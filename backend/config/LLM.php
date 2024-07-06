61c455ba97656e4086765a496835fb5df9688cf4
xtcVFIlH <hoccl@sina.com>
2024-07-06 16:45:35 +0800

    删除LLM组件，并配置Gemini组件

--- backend/config/LLM.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'apiKey' => $_ENV['API_KEY'],
-    'proxy' => $_ENV['PROXY'],
-    'usingFrontendProxy' => !!$_ENV['FRONTEND_PROXY'],
-    'uri' => 'https://generativelanguage.googleapis.com/v1beta/',
-];
